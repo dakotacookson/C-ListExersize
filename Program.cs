79056390f55bbbfbe73d3fdef31c54f25c2e889b
unknown <dakota.cookson1995@gmail.com>
2019-03-04 09:41:54 -0600

    Added c# lists

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+namespace NSSOrientation {
+    class Program {
+        public static void Main ()
+
+        {
+
+            List<string> finalplanetList = new List<string> () {
+                "Neptune",
+                "Uranus",
+            };
+
+            List<string> planetList = new List<string> () {
+                "Mercury",
+                "Mars"
+            };
+
+            planetList.Add ("Jupiter");
+            planetList.Add ("Saturn");
+            planetList.AddRange (finalplanetList);
+            planetList.Insert (1, "venus");
+            planetList.Insert (2, "Earth");
+            planetList.Add ("Pluto");
+            planetList.ForEach (planet => Console.WriteLine (planet + " ")); {
+                List<string> rockyPlanets = planetList.GetRange (0, 4);
+                Console.WriteLine ();
+                foreach (string rockyPlanet in rockyPlanets) {
+                    Console.WriteLine (rockyPlanet + " " + "are rocky");
+                }
+                planetList.RemoveAt (8);
+                planetList.ForEach (planet => Console.WriteLine (planet)); {
+                }
+            }
+        }
+    }
+}
